5e05fa90ff0c45e30043378542fb7419696e9241
s30718 <s30718@pjwstk.edu.pl>
2025-06-08 16:49:12 +0200

    Del

--- Kolokwium2GrupaB/Kolokwium2GrupaB/Services/IDbService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kolokwium2GrupaB.DTOs;
-
-namespace Kolokwium2GrupaB.Services;
-
-public interface IDbService
-{
-    Task<CustomerDTO> GetCustomerById(int CustomerId);   
-    Task<CustomerAddDTO> AddCustomer(CustomerAddDTO customer);
-}
