65d0c01b23d43eaf20455f6fb0d90e2b7c666dc0
s30718 <s30718@pjwstk.edu.pl>
2025-06-11 16:00:54 +0200

    Usuwanie zbędnych plików z repository

--- ExampleTest2/Services/IDbService.cs
+++
@@ -1,9 +0,0 @@
-using ExampleTest2.DTOs;
-
-namespace ExampleTest2.Services;
-
-public interface IDbService
-{
-    Task<OrderDto> GetOrderById(int orderId);
-    Task FulfillOrder(int orderId, FulfillOrderDto dto);
-}
